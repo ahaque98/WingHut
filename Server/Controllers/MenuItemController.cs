54fe4e5f57ae97caf14ea127ad4e2ee78dabaa90
vikingslord <vikingslord98@gmail.com>
2023-08-11 16:08:14 +0600

    Added APi Response

---
+++ Server/Controllers/MenuItemController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Models.Response;
+
+namespace Server.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly ApiResponse _response;
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result = await _db.MenuItems.ToListAsync();
+
+            _response.StatusCode = System.Net.HttpStatusCode.OK;
+
+            return Ok(_response);
+        }
+    }
+}
---
+++ Server/Models/Response/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Server.Models.Response
+{
+    public class ApiResponse
+    {
+        public ApiResponse()
+        {
+            ErrorMessage = new List<string>();
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+        public bool isSuccess { get; set; } = true;
+        public List<string> ErrorMessage { get; set; }
+        public object Result { get; set; }
+    }
+}
