2e29e754413d8cd57622b2e880858b8dfa205758
vikingslord <vikingslord98@gmail.com>
2023-08-13 19:24:06 +0600

    Added CartItem & ShoppingCart

---
+++ Server/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+
+        public string StripePaymentIntentId { get; set; }
+        public string ClientSecret { get; set; }
+
+        ICollection<CartItem> CartItems { get; set; }
+
+        [NotMapped]
+        public double CartTotal { get; set; }
+
+    }        
+}
